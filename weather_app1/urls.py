603da9a37f0caffcf499ab03abc9438474eac9d4
RedaRahmani <101414176+RedaRahmani@users.noreply.github.com>
2023-12-14 01:30:28 +0100

    add all python file

---
+++ weather_app1/urls.py
@@ -0,0 +1,9 @@
+# urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.forecast, name='forecast'),
+    path('about/', views.about, name='about'),  # Update the URL and name
+    path('login/', views.login, name='login') # Update the URL and name
+]
